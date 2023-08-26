63fa76dcf68608b8b65f4ec48333e25ff6ab8acb
Murali07 <muraliselvaraj97@gmail.com>
2023-08-27 00:15:04 +0530

    updated the backend deployed url

--- src/App.js
+++ src/App.js
@@ -22,7 +22,8 @@ function App() {
     const user = useSelector((state) => state.user);
     const dispatch = useDispatch();
     useEffect(() => {
-        const socket = io("ws://localhost:8080");
+        // const socket = io("ws://localhost:8080");
+        const socket = io("ws://ecommerce-backend-bird.onrender.com");
         socket.off("notification").on("notification", (msgObj, user_id) => {
             // logic for notification
             if (user_id === user._id) {
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080",
+    // baseURL: "http://localhost:8080",
+    baseURL: "https://ecommerce-backend-bird.onrender.com",
 });
 
 export default instance;
